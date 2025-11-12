58df627cfec733f379e01de2101471823e8462a2
pper901 <rickybastet@gmail.com>
2025-11-12 09:38:36 +0100

    Fix APP_URL for HTTPS

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    server: {
+        https: true,
+        host: '0.0.0.0',
+    },
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
@@ -10,4 +18,5 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
+    base: process.env.VITE_APP_URL ? process.env.VITE_APP_URL + '/' : '/',
 });
