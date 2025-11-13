3860b401393975aed70588fb1f0501b61cba3154
pper901 <rickybastet@gmail.com>
2025-11-13 07:17:30 +0100

    Changed the layout app file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Bellitek | The Only Tech Store You Need</title>
-  @vite('resources/css/app.css')
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-50 text-gray-900">
 
--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    base: process.env.VITE_APP_URL + '/build/',
+    base: '/build/',
 });
