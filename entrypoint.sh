e27911ab51abb7fc3d7980d9336819c1fc007046
pper901 <rickybastet@gmail.com>
2025-12-18 19:29:00 +0100

    This is version 1.66

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,7 +19,7 @@ php artisan key:generate --force
 
 # 3. Run Database Migrations
 echo "Running database migrations..."
-php artisan migrate:fresh --force
+php artisan migrate:fresh
 
 # 4. Cache configuration
 echo "Rebuilding configuration and routes..."
