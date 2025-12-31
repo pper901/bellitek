0b0013de5e279cd2a793ab64cdfa1acb7c72e6f7
pper901 <rickybastet@gmail.com>
2025-12-31 12:05:20 +0100

    This is the payment process v1.13

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
 
 class CheckoutController extends Controller
 {
